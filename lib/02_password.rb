9306a4cb5a9c747036b1985d21d37861efe441d1
Raphael-dln <delannay.raphael@gmail.com>
2020-04-08 19:22:38 +0200

    ajout de la dernière méthode

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -14,12 +14,20 @@ def login
     $login = gets.chomp
   end
 
-puts "Vous accédez à la page secrète de la NASA !"
+puts "Vous allez maintenant accéder à la page secrète de la NASA !"
+end
+
+def welcome_screen
+  if $password == $login
+    puts " Information confidentielle #1"
+    puts " L'Homme n'est jamais allé sur la Lune"
+  end
 end
 
 def perform
   define_password
   login
+  welcome_screen
 end
 
 perform
